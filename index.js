3e44cc3967a97ef4fd3a954560359f576d773a66
t4nny <tanveerali307@gmail.com>
2020-09-16 14:57:02 +0530

    code updated- whole code working fine

--- index.js
+++ index.js
@@ -1,98 +1,88 @@
-const express = require('express')
-const app = express()
-const  { body, validationResult} = require('express-validator')
+const express = require("express");
+const app = express();
+const { body, validationResult } = require("express-validator");
 const bin = require("binary-code");
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }));
+const bodyParser = require("body-parser");
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 //set the template engine ejs
-app.set('view engine', 'ejs')
-
+app.set("view engine", "ejs");
 
 //routes
-app.get('/', (req, res) => {
-	res.render('index')
-})
+app.get("/", urlencodedParser, (req, res) => {
+  var convertit = req.body.convertedText;
+  var againPlainText;
+  res.render("index", { convertit: convertit, againPlain: againPlainText });
+});
 
+//app.locals.data1 = "Hello WOrld";
 
-app.locals.data1="Hello WOrld" 
+app.post("/convert", urlencodedParser, function (req, res) {
+  var plaintxt = req.body.plaintext;
 
-app.post('/convert', function (req, res){
+  //res.send(`<script>alert('Clicked Successful! Click Ok To open git repo.'); {window.location.href = 'https://github.com/t4nny/B1nCrypti0n';}</script>`)
 
-    var plaintxt= req.body.plaintext;
-    var convertit = req.body.convertedText;
-    
-    //res.send(`<script>alert('Clicked Successful! Click Ok To open git repo.'); {window.location.href = 'https://github.com/t4nny/B1nCrypti0n';}</script>`)
-    
-    
-    // for(var i=0; i<plaintxt.length; i++){
-    //     res.send(plaintxt.charAt(i))
-    // }
-    
-    //res.send(plaintxt.split(''))
-    
-    var arr=new Array (bin.binary(plaintxt))
-    //res.send(bin.binary(plaintxt).charAt(8).split('').join(' '))
-    
-    for(var i=0; i<arr.length;i++)
-    {
-        var io = arr[i]
-        console.log(io + " ")
+  // for(var i=0; i<plaintxt.length; i++){
+  //     res.send(plaintxt.charAt(i))
+  // }
+
+  //res.send(plaintxt.split(''))
+
+  var arr = new Array(bin.binary(plaintxt));
+  //res.send(bin.binary(plaintxt).charAt(8).split('').join(' '))
+
+  for (var i = 0; i < arr.length; i++) {
+    var io = arr[i];
+    console.log(io + " ");
+  }
+
+  // arr.forEach(element => {
+  //     var io = arr[element]
+  //     console.log(io)
+  // });
+  //res.send(bin.text('0101010001100001011011100110111001111001'))
+
+  // for (var i=0; i<arr.length;i++) {
+  //     res.send(arr[i] + "<br>")
+  //     }
+
+  var tmp = bin.binary(plaintxt).split("");
+  console.log(tmp);
+
+  var emparr = new Array();
+
+  for (i = 0; i < tmp.length; i++) {
+    var tmp2 = tmp[i];
+    if (tmp2 == 0) {
+      emparr.push("b0nod");
+    } else {
+      emparr.push("b1nod");
     }
-    
-    // arr.forEach(element => {
-    //     var io = arr[element]
-    //     console.log(io)
-    // });
-    //res.send(bin.text('0101010001100001011011100110111001111001'))
-    
-    // for (var i=0; i<arr.length;i++) {
-    //     res.send(arr[i] + "<br>")
-    //     }
-    
-    var tmp= bin.binary(plaintxt).split('')
-    console.log(tmp)
-    
-    var emparr=new Array();
-    
-    for(i=0;i<tmp.length;i++){
-        var tmp2=tmp[i];
-        if(tmp2==0){
-           emparr.push("b0nod")
-        
-        }else{
-            emparr.push("b1nod")
-        }
+  }
+
+  console.log(emparr);
+
+  convertit = emparr.join(" ");
+
+  var emparr2 = new Array();
+
+  for (i = 0; i < emparr.length; i++) {
+    var tmp3 = emparr[i];
+    if (tmp3 == "b0nod") {
+      emparr2.push(0);
+    } else {
+      emparr2.push(1);
     }
-    
-    
-    console.log(emparr)
+  }
 
-    
-    convertit = emparr.join(" ")
-    
-       
-    
-    var emparr2=new Array();
-        
-        for(i=0;i<emparr.length;i++){
-            var tmp3=emparr[i];
-            if(tmp3=="b0nod"){
-               emparr2.push(0)
-            
-            }else{
-                emparr2.push(1)
-            }
-        }
-    
-        
-        var finaldata= emparr2.join(" ")
-    
-    
-    
-    })
+  var finaldata = emparr2.join(" ");
+  againPlainText = bin.text(finaldata);
+  console.log(againPlainText);
+  res.render("index", { convertit: convertit, againPlain: againPlainText });
+});
 
-   
-    
-    let port_number = process.env.PORT || 3000;
-    server=app.listen(port_number, () => console.log("Server is Running on PORT "+ port_number));
-    
+let port_number = process.env.PORT || 3001;
+server = app.listen(port_number, () =>
+  console.log("Server is Running on PORT " + port_number)
+);
