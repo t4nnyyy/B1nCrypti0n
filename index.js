5e4cadd9d485e2e968df3e607b22c0002afade25
t4nny <tanveerali307@gmail.com>
2020-09-19 22:32:07 +0530

    optimized for mobile view

--- index.js
+++ index.js
@@ -10,27 +10,36 @@ app.set("view engine", "ejs");
 
 //routes
 app.get("/", urlencodedParser, (req, res) => {
-  var convertit = req.body.convertedText;
+  var convertit = req.body.frm1txtConverted;
   var againPlainText;
+
+  var newconvertit = req.body.frm2txtConverted;
+  var newplaintext;
+
   res.render("index", {
     convertit: convertit,
     againPlain: againPlainText,
     plaintext: againPlainText,
+
+    newconvertit: newconvertit,
+    newplaintext: newplaintext,
   });
 });
 
-app.post("/convert", urlencodedParser, function (req, res) {
-  var plaintxt = req.body.plaintext;
+app.post("/Encrypt", urlencodedParser, function (req, res) {
+  var plaintxt = req.body.frm1txtPlain;
+  var newconvertit = req.body.frm2txtConverted;
+  var newplaintext;
 
   var arr = new Array(bin.binary(plaintxt));
 
   for (var i = 0; i < arr.length; i++) {
     var io = arr[i];
-    console.log(io + " ");
+    //console.log(io + " ");
   }
 
   var tmp = bin.binary(plaintxt).split("");
-  console.log(tmp);
+  //console.log(tmp);
 
   var emparr = new Array();
 
@@ -43,11 +52,13 @@ app.post("/convert", urlencodedParser, function (req, res) {
     }
   }
 
-  console.log(emparr);
+  //console.log(emparr);
 
   convertit = emparr.join(" ");
 
   var emparr2 = new Array();
+
+  // console.log(emparr.length)
 
   for (i = 0; i < emparr.length; i++) {
     var tmp3 = emparr[i];
@@ -60,11 +71,60 @@ app.post("/convert", urlencodedParser, function (req, res) {
 
   var finaldata = emparr2.join(" ");
   againPlainText = bin.text(finaldata);
-  console.log(againPlainText);
+  //console.log(againPlainText);
+
   res.render("index", {
     convertit: convertit,
     againPlain: againPlainText,
     plaintext: againPlainText,
+
+    newconvertit: newconvertit,
+    newplaintext: newplaintext,
+  });
+});
+
+app.post("/Decrypt", urlencodedParser, function (req, res) {
+  var convertit = req.body.frm1txtConverted;
+  var newconvertit = req.body.frm2txtConverted;
+  var newplaintext;
+  var againPlainText;
+
+  var newarr = newconvertit;
+
+  var emparr3 = new Array();
+  emparr3 = newarr.split(" ");
+
+  var emparr4 = new Array();
+
+  for (i = 0; i < emparr3.length; i++) {
+    var tmp3 = emparr3[i];
+    if (tmp3 == "b0nod") {
+      emparr4.push(0);
+    } else {
+      emparr4.push(1);
+    }
+  }
+
+  //console.log(againPlainText);
+
+  var finaldata = emparr4.join(" ");
+  newplaintext = bin.text(finaldata);
+  //console.log(newplaintext);
+  //console.log(finaldata);
+  //console.log(emparr4);
+  //console.log("emparr 3 " + emparr3);
+  //console.log("new arr " + newarr);
+  //console.log("new converted " + newconvertit);
+
+  //console.log("is this wrking");
+
+  res.render("index", {
+    convertit: convertit,
+    againPlain: againPlainText,
+    plaintext: againPlainText,
+
+    newconvertit: newconvertit,
+    newplaintext: newplaintext,
   });
 });
 
